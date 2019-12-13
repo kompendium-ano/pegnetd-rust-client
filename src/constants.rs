80a459426ecbb2f9a9f20edfb43aff47319c680b
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-12-13 11:43:03 +1100

    fixed constants

--- src/constants.rs
+++ src/constants.rs
@@ -3,6 +3,6 @@ pub const JSONRPC : &str = "2.0";
 /// Default JSON-RPC ID
 pub const ID: usize = 0;
 /// Default local node
-pub const LOCAL_NODE: &str = "http://localhost:8070";
+pub const LOCAL_NODE: &str = "http://localhost:8070/v1";
 /// Open pegnetd node
 pub const OPEN_NODE: &str = "https://api.pegnetd.com";
