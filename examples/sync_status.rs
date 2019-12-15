d0ed68225441be91981db0d6d4345b74d09bad0b
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-12-16 10:54:29 +1100

    update example

--- examples/sync_status.rs
+++ examples/sync_status.rs
@@ -1,7 +1,7 @@
 use pegnetd::*;
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() -> Result<()> {
   let api = Pegnetd::open_node();
   let response = sync_status(&api).await;
   assert!(response.result.syncheight > 0);
