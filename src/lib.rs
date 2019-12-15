1394de05e88474da16c030ba18efb676aed1e8d8
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-12-16 10:54:50 +1100

    re-export tokio proc macros

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@ pub mod responses;
 pub mod api;
 pub mod constants;
 
+#[macro_use]
 pub use tokio::prelude::*;
 pub use tokio::runtime::Runtime;
 pub use constants::*;
 pub use requests::*;
 pub use responses::*;
 pub use api::*;
+pub use reqwest::*;
 
 use serde_json::json;
